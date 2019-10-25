2b563f1f24697c8b5c7b7334b6867662d4af70b0
Luiz <luiz.felipe.556@outlook.com>
2019-10-25 09:15:01 -0300

    tests

--- lib/tp2.cpp
+++ lib/tp2.cpp
@@ -7,15 +7,16 @@ using namespace std;
 
 int main() {
     
-    Greedy g = Greedy(4,2250);
-    g.addIsland(1000,60);
-    g.addIsland(50,2);
-    g.addIsland(100,5);
-    g.addIsland(2000, 70);
+    Greedy g = Greedy(5,6000);
+    g.addIsland(1000,30);
+    g.addIsland(2000,32);
+    g.addIsland(500,4);
+    g.addIsland(5000, 90);
+    g.addIsland(2200,45);
 
     g.orderByGreaterCostBenefit();
     g.run();
-    
+
     g.run_dynamic();
     
     return 0;
